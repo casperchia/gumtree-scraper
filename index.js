7a88bd0c54bb847c7319672c83c7723281948958
Casper Chia <casperchiakyechuen@gmail.com>
2017-02-26 01:55:19 +1100

    Add config file and emailing feature

--- index.js
+++ index.js
@@ -6,11 +6,12 @@
 
 const scrapeIt = require("scrape-it");
 const jsonfile = require("jsonfile");
+const nodemailer = require("nodemailer");
+const CONFIG = require("./config.json");
 
 const filePath = './data.json'
-const searchQuery = 'brazil argentina'
+const searchQuery = CONFIG.searchQuery;
 const url = `http://www.gumtree.com.au/s-${searchQuery}/k0`;
-
 // http://www.gumtree.com.au/s-brazil+argentina/page-2/k0?fromSearchBox=true
 
 var scrapeModel = {
@@ -36,6 +37,7 @@ var scrapeModel = {
    }
 }
 
+console.log('Retrieving data...');
 scrapeIt(url, scrapeModel, process);
 
 function process(err, results) {
@@ -58,6 +60,14 @@ function process(err, results) {
    }
 
    jsonfile.writeFileSync(filePath, existingAds, {spaces: 2});
+
+   var newAdsCount = Object.keys(newAds).length;
+   if (newAdsCount) {
+      console.log(`${newAdsCount} new ads found`);
+      sendMail(newAds);
+   } else {
+      console.log("No new ads found");
+   }
 }
 
 function toDictionary(results) {
@@ -77,4 +87,44 @@ function toDictionary(results) {
       }
    })
    return dict;
+}
+
+function sendMail(ads) {
+   console.log('Sending mail...');
+
+   var adsCount = Object.keys(ads).length;
+   var textContent = `${adsCount} new ad(s): \n`;
+   var adsHtml = `<p>${adsCount} new ad(s):</p>\n`;
+   for (var id in ads) {
+      textContent = textContent.concat(`${ads[id].title} - ${ads[id].url}\n`);
+      adsHtml = adsHtml.concat(`<p>${ads[id].price} - <a href="${ads[id].url}">${ads[id].title}</a></p>\n`);
+   }
+
+   var htmlContent = ''.concat("<html><head></head><body>", adsHtml, "</body></html>");
+
+   var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+         user: CONFIG.senderEmail,
+         pass: CONFIG.senderPassword
+      }
+   });
+
+   // setup email data with unicode symbols
+   var mailOptions = {
+      from: `"${CONFIG.senderName}" <${CONFIG.senderEmail}>`,
+      to: CONFIG.recipients,
+      subject: `New ${searchQuery} Ads`,
+      text: textContent,
+      html: htmlContent
+   };
+
+   // send mail with defined transport object
+   transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+         return console.log(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+   });
+
 }
